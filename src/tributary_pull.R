72082b6d437da367b0e19b7668aefc1719119a3c
Ryan-Riggs <ryanriggs7@gmail.com>
2024-05-07 09:11:53 -0500

    Update tributary pull to an example function.

--- src/tributary_pull.R
+++ src/tributary_pull.R
@@ -1,1 +1,13 @@
-'https://geoglows.ecmwf.int/api/v2/retrospective/710825777?format=csv&start_date=20231215'
+library(httr)
+
+reach = '710825777'
+pass_start_date = '20231215'
+
+download_tributary = function(reach_id){
+  url = 'https://geoglows.ecmwf.int/api/v2/retrospective/'
+  website = paste0(url, reach_id, '?format=csv&start_date=', pass_start_date)
+  data=content(GET(website))
+  return(data)
+}
+
+testing = download_tributary(reach)
